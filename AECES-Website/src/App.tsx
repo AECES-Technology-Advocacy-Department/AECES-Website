7e776ad94ba8b9ce6dfd3707eb670abf687f7525
CarlSiy <siycarlwashington@gmail.com>
2024-03-06 11:59:47 +0800

    first-commit

--- AECES-Website/src/App.tsx
+++ AECES-Website/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Hello world <code>src/App.tsx</code>
+          WHats up <code>src/App.tsx</code>
         </p>
       </div>
       <p className="read-the-docs">
